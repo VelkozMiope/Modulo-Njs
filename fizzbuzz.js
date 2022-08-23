abd451735e139a1ff5cdc62e547a4687db324a06
VelkozMiope <kinoreckclockwork@hotmail.com>
2022-08-23 17:50:32 +0000

    Simplificação do Fizzbuzz

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,19 +1,15 @@
-function fizzbuzz(max) {
-    for (let c = 1; c <= max; c++) {
-        let mensagem = ""
-        if (c % 3 === 0) {
-            mensagem = mensagem + "Fizz";
-
-        }
-        if (c % 5 === 0) {
-            mensagem = mensagem += "Buzz";
-
-        }
-        if (mensagem.length === 0) {
-            mensagem = String(c);
-        }
-        console.log(mensagem);
-    };
-};
+function fizzbuzz(numero) {
+    let mensagem = "";
+    if (numero % 3 === 0) {
+        mensagem = mensagem + "Fizz";
+    }
+    if (numero % 5 === 0) {
+        mensagem = mensagem + "Buzz";
+    }
+    if (mensagem.length === 0) {
+        mensagem = String(numero);
+    }
+    return mensagem;
+}
 
 module.exports = fizzbuzz

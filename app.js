f883136897f4b3f24b8fb15b0baa34c53f1f73e0
VelkozMiope <kinoreckclockwork@hotmail.com>
2022-08-23 18:19:27 +0000

    Remoção do servidor.js e mudança do app.js

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-const fs = require ('fs').promises
-const path = require('path')
-const fizzbuzz = require("./fizzbuzz")
-main()
-async function main(){
-    const filepath = path.join(__dirname, 'input.txt')
-    const dados = await fs.readFile(filepath, 'utf8');
-    await fs.appendFile("output.txt", dados+'\n')
-    const max = Number(dados)
-    fizzbuzz(max)
-};
+const http = require('http');
+
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Teste');
+});
+
+server.listen(port, () => {
+    console.log(`Servidor ouvindo na porta ${port}`);
+});

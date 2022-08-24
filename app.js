3f2bc35c48655f78c80365e62a51628eea855ba4
VelkozMiope <kinoreckclockwork@hotmail.com>
2022-08-24 17:46:16 +0000

    testes

--- app.js
+++ app.js
@@ -5,9 +5,12 @@ const port = 3000
 app.use(express.static(`${__dirname}/public`))
 
 app.get('/', (req, res) => {
-    res.sendFile(`${__dirname}/index.html`)
-  })
+  res.sendFile(`${__dirname}/index.html`)
+})
 
-  app.listen(port, () => {
+app.get('/transacoes', (req, res) => {
+  res.sendFile(`${__dirname}/transacoes.html`)
+})
+app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
